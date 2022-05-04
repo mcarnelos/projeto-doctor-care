dbbd8314e7f0cea7dfee3a460edf07716570bed1
Murilo Carnelos <muriloc.c@hotmail.com>
2022-05-04 20:22:25 -0300

    Ajustes no menu expanded

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-function onScroll() {
+function onScroll() { 
     if(scrollY > 0) {
-    navigation.classList.add('scroll')
+    navigation.classList.add('scroll') /* adiciona a classe scroll ao nav quando faz scroll*/
     } else{
     navigation.classList.remove('scroll')
     }
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded') /* adiciona a classe menu-expanded ao body quando o menu é aberto*/
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded') /* remove a classe menu-expanded quando o menu é fechado */
 }

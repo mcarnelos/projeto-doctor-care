473c6521d7cde4985d1828642f13af08a7adde49
Murilo Carnelos <muriloc.c@hotmail.com>
2022-05-05 22:25:31 -0300

    criacao do contact e ajustes no layout

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-function onScroll() { 
+window.addEventListener('scroll', onScroll) /* adiciona o evento scroll executando a função onScroll*/
+
+onScroll() /* executa a função, pos não esta mais no body */
+
+function onScroll() { /* gerencia os scrolls da pagina */
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() { /* mostra o nav ao fazer scroll */
     if(scrollY > 0) {
     navigation.classList.add('scroll') /* adiciona a classe scroll ao nav quando faz scroll*/
     } else{
     navigation.classList.remove('scroll')
+    }
+}
+
+function showBackToTopButtonOnScroll() {
+    if(scrollY > 400) {
+    backToTopButton.classList.add('show') /* adiciona a classe show quando passa de 500*/
+    } else{
+    backToTopButton.classList.remove('show')
     }
 }
 
